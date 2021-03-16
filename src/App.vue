2ddfe1949c98b955269c9977b6b1ff6000208618
abir006 <39681215+abir006@users.noreply.github.com>
2021-03-16 13:44:54 +0200

    closed #1, Header implemented.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="jumbotron">
+    &nbsp;
+    <h1 style="color: whitesmoke">Field Capture</h1>
+    &nbsp;
+    <h3 style="color: #2c3e50">A demo game showcasing AI and basic web programing</h3>
+    <h5 style="color: #2c3e50">The objective is to capture more field (making more steps) than the rival</h5>
+    &nbsp;
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.jumbotron {
+  background-color: #1abc9c; /* Orange */
+}
+</style>
+
